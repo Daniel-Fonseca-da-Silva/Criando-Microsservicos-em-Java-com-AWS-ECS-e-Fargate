ac3f39320376e73a0998accb0094cea4e1a699da
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-12 21:33:12 -0300

    Created repository product

--- src/main/java/br/com/siecola/aws_project01/model/Product.java
+++ src/main/java/br/com/siecola/aws_project01/model/Product.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Entity
 public class Product {
     @Id
-    @Generated(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(length = 32, nullable = false)
---
+++ src/main/java/br/com/siecola/aws_project01/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package br.com.siecola.aws_project01.repository;
+
+import br.com.siecola.aws_project01.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+    Optional<Product> findByCode(String code);
+}
